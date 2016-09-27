dce1ccd2bf20302f16630f9cf1b2aecca0220003
acoshift <acoshift@gmail.com>
2016-09-27 22:32:27 +0700

    Fix loading

--- src/components/course-chat.vue
+++ src/components/course-chat.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ui basic segment" :class="{loading: loading <= 0}">
+  <div class="ui basic segment" :class="{loading: loading > 0}">
     <div class="ui massive breadcrumb">
       <router-link class="section" to="/home">Courses</router-link>
       <i class="right chevron icon divider"></i>
