2d4d3252b6cc3750da2f837b487281c05a6d8bf5
acoshift <acoshift@gmail.com>
2016-10-15 17:15:50 +0700

    Move firebase config to env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,10 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  FIREBASE: `{
+    apiKey: 'AIzaSyBa6jPVcSPbLzjuei9-d0u9q7M-9rGjmb8',
+    authDomain: 'acourse-d9d0a.firebaseapp.com',
+    databaseURL: 'https://acourse-d9d0a.firebaseio.com',
+    storageBucket: 'acourse-d9d0a.appspot.com',
+    messagingSenderId: '582047384847'
+  }`
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -14,13 +14,7 @@ export default {
   currentUser: new BehaviorSubject(),
 
   init () {
-    firebase.initializeApp({
-      apiKey: 'AIzaSyBa6jPVcSPbLzjuei9-d0u9q7M-9rGjmb8',
-      authDomain: 'acourse-d9d0a.firebaseapp.com',
-      databaseURL: 'https://acourse-d9d0a.firebaseio.com',
-      storageBucket: 'acourse-d9d0a.appspot.com',
-      messagingSenderId: '582047384847'
-    })
+    firebase.initializeApp(process.env.FIREBASE)
 
     firebase.auth().onAuthStateChanged((user) => {
       this.currentUser.next(user)
