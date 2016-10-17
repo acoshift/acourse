f03c310db5c9c0e70f36063a02610aee969f3383
acoshift <acoshift@gmail.com>
2016-10-17 23:07:19 +0700

    Add push noti script

---
+++ send-noti.js
@@ -0,0 +1,37 @@
+const firebase = require('firebase')
+const _ = require('lodash')
+const request = require('request')
+
+firebase.initializeApp({
+  serviceAccount: './.private/acourse-e38f3d69ac48.json',
+  databaseURL: 'https://acourse-d9d0a.firebaseio.com'
+})
+
+const userRef = firebase.database().ref('notification')
+
+userRef.once('value', (snapshot) => {
+  const val = snapshot.val()
+  const keys = _.keys(val)
+  request.post('https://fcm.googleapis.com/fcm/send', {
+    headers: {
+      Authorization: 'key='
+    },
+    json: {
+      notification: {
+        title: 'Acourse',
+        body: '',
+        click_action: ''
+      },
+      data: {
+        color: 'info',
+        body: '',
+        timeout: 10000
+      },
+      registration_ids: keys
+    }
+  }, (err, resp, body) => {
+    if (err) return console.log(err)
+    console.log(body)
+    process.exit(0)
+  })
+})
