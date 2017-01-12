0f7e91290153e4cba223ecf3c89e422737b5e464
acoshift <acoshift@gmail.com>
2017-01-12 10:02:37 +0700

    Reload course data if sign in on course view

--- ui/src/components/CourseView.vue
+++ ui/src/components/CourseView.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import { Course } from 'services'
+import { Course, Auth } from 'services'
 import CourseHeader from './CourseHeader'
 import CourseVideo from './CourseVideo'
 import CourseDetail from './CourseDetail'
@@ -32,7 +32,10 @@ export default {
   subscriptions () {
     return {
       course: this.$$route
-        .flatMap((route) => Course.get(route.params.id))
+        .flatMap((route) => Course.get(route.params.id)),
+      currentUser: Auth.currentUser()
+        .flatMap(() => this.$$route.first())
+        .do((route) => Course.fetch(route.params.id))
     }
   }
 }
