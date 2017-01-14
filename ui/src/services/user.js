6001b352569756e01042625b7001872bd917652f
acoshift <acoshift@gmail.com>
2017-01-15 03:02:25 +0700

    Update ui user service

--- ui/src/services/user.js
+++ ui/src/services/user.js
@@ -1,23 +1,22 @@
 import Firebase from './firebase'
-import API from './api'
+import RPC from './rpc'
 import orderBy from 'lodash/fp/orderBy'
+import response from './response'
 
 export default {
   get (id) {
-    return API.get(`/user/${id}`)
+    return RPC.invoke('/acourse.UserService/GetUser', { userId: id })
   },
   ownCourses (id) {
-    return API.get(`/course?owner=${id}`)
+    return RPC.invoke(`/course?owner=${id}`)
       .map(orderBy(['createdAt'], ['desc']))
   },
   courses (id) {
-    return API.get(`/course?student=${id}`)
+    return RPC.invoke('/acourse.CourseService/ListOwnCourses', { userId: id })
+      .map(response.courses)
       .map(orderBy(['createdAt'], ['desc']))
   },
   upload (id, file) {
     return Firebase.upload(`user/${id}/${Date.now()}`, file)
-  },
-  update (id, data) {
-    return Firebase.update(`user/${id}`, data)
   }
 }
