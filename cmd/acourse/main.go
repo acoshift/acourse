67cc837a3b57bd584599603dc3d11801148e5953
acoshift <acoshift@gmail.com>
2017-01-26 20:08:56 +0700

    remove some tls config

--- cmd/acourse/main.go
+++ cmd/acourse/main.go
@@ -205,15 +205,15 @@ func main() {
 			})))
 		}()
 		tlsConfig := &tls.Config{
-			MinVersion:               tls.VersionTLS12,
+			// MinVersion:               tls.VersionTLS12,
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
+			// CipherSuites: []uint16{
+			// 	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			// 	tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			// 	tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			// 	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			// },
 		}
 		tlsServer := &http.Server{
 			Addr:      tlsAddr,
