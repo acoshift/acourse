e2a277db8ea49ba2f37c7d6e19c222b038ad6261
acoshift <acoshift@gmail.com>
2017-01-26 20:28:30 +0700

    ui: reduce lazy load component

--- ui/src/components/index.js
+++ ui/src/components/index.js
@@ -1,12 +1,11 @@
 import Layout from './Layout'
-const Home = () => System.import('./Home')
-const Profile = () => System.import('./Profile')
-const ProfileEdit = () => System.import('./ProfileEdit')
-const CourseEditor = () => System.import('./CourseEditor')
-const CourseView = () => System.import('./CourseView')
-// import CourseView from './CourseView'
+import Home from './Home'
+import Profile from './Profile'
+import ProfileEdit from './ProfileEdit'
+import CourseEditor from './CourseEditor'
+import CourseView from './CourseView'
 // import UserView from './UserView'
-const Course = () => System.import('./Course')
+import Course from './Course'
 // import CourseAttend from './CourseAttend'
 import CourseAssignments from './CourseAssignments'
 import CourseAssignmentEdit from './CourseAssignmentEdit'
