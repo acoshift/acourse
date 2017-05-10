ea29856ff9e2c5416c10e2a9e58146c43fc9ad50
acoshift <acoshift@gmail.com>
2017-05-10 17:11:50 +0700

    add accept and reject payment

--- pkg/model/enroll.go
+++ pkg/model/enroll.go
@@ -6,14 +6,16 @@ import (
 	"github.com/garyburd/redigo/redis"
 )
 
-// Enroll an user to a course
-func Enroll(c redis.Conn, userID, courseID string) error {
-	c.Send("MULTI")
-
+func enroll(c redis.Conn, userID, courseID string) {
 	now := time.Now().UnixNano()
 	c.Send("ZADD", key("u", userID, "e"), now, courseID)
 	c.Send("ZADD", key("c", courseID, "u"), now, userID)
+}
 
+// Enroll an user to a course
+func Enroll(c redis.Conn, userID, courseID string) error {
+	c.Send("MULTI")
+	enroll(c, userID, courseID)
 	_, err := c.Do("EXEC")
 	if err != nil {
 		return err
--- pkg/model/payment.go
+++ pkg/model/payment.go
@@ -1,6 +1,11 @@
 package model
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
 
 // Payment model
 type Payment struct {
@@ -26,4 +31,83 @@ const (
 // ID returns payment id
 func (x *Payment) ID() string {
 	return x.id
+}
+
+func (x *Payment) save(c redis.Conn) {
+	x.UpdatedAt = time.Now()
+	if x.CreatedAt.IsZero() {
+		x.CreatedAt = x.UpdatedAt
+		n := x.CreatedAt.UnixNano()
+		c.Send("ZADD", key("p", "t0"), n, x.id)
+		c.Send("ZADD", key("u", x.UserID, "p"), n, x.id)
+		c.Send("SADD", key("c", x.CourseID, "p"), n, x.id)
+	}
+
+	c.Send("ZADD", key("p", "t1"), x.UpdatedAt.UnixNano(), x.id)
+	c.Send("HSET", key("p"), x.id, enc(x))
+}
+
+// Save saves payment
+func (x *Payment) Save(c redis.Conn) error {
+	if len(x.UserID) == 0 {
+		return fmt.Errorf("invalid user")
+	}
+	if len(x.CourseID) == 0 {
+		return fmt.Errorf("invalid course")
+	}
+
+	var err error
+	if len(x.id) == 0 {
+		x.id, err = redis.String(c.Do("INCR", key("id", "p")))
+		if err != nil {
+			return err
+		}
+	}
+
+	c.Send("MULTI")
+	c.Send("SADD", key("p", "all"), x.id)
+	x.save(c)
+	_, err = c.Do("EXEC")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Accept accepts payment and save
+func (x *Payment) Accept(c redis.Conn) error {
+	// TODO: accept and reject should run in transaction in `watch`
+	if len(x.id) == 0 {
+		return fmt.Errorf("invalid payment")
+	}
+	if x.Status != Pending {
+		return fmt.Errorf("invalid payment status")
+	}
+	x.Status = Accepted
+	c.Send("MULTI")
+	enroll(c, x.UserID, x.CourseID)
+	x.save(c)
+	_, err := c.Do("EXEC")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Reject rejects a payment
+func (x *Payment) Reject(c redis.Conn) error {
+	if len(x.id) == 0 {
+		return fmt.Errorf("invalid payment")
+	}
+	if x.Status != Pending {
+		return fmt.Errorf("invalid payment status")
+	}
+	x.Status = Rejected
+	c.Send("MULTI")
+	x.save(c)
+	_, err := c.Do("EXEC")
+	if err != nil {
+		return err
+	}
+	return nil
 }
