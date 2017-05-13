91fb6c82cafd54eac80be9c8ef5aada36d74b8d2
acoshift <acoshift@gmail.com>
2017-05-13 19:08:00 +0700

    add assignment model

---
+++ pkg/model/assignment.go
@@ -0,0 +1,67 @@
+package model
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+// Assignment model
+type Assignment struct {
+	id        string
+	Title     string
+	Desc      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// ID returns assignment id
+func (x *Assignment) ID() string {
+	return x.id
+}
+
+// Save saves assignment
+func (x *Assignment) Save(c redis.Conn) error {
+	if len(x.id) == 0 {
+		id, err := redis.Int64(c.Do("INCR", key("id", "a")))
+		if err != nil {
+			return err
+		}
+		x.id = strconv.FormatInt(id, 10)
+	}
+
+	x.UpdatedAt = time.Now()
+	if x.CreatedAt.IsZero() {
+		x.CreatedAt = x.UpdatedAt
+	}
+	_, err := c.Do("HSET", key("a"), x.id, enc(x))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetAssignments gets assignments
+func GetAssignments(c redis.Conn, assignmentIDs []string) ([]*Assignment, error) {
+	xs := make([]*Assignment, len(assignmentIDs))
+	infs := make([]interface{}, len(assignmentIDs)+1)
+	infs[0] = key("a")
+	for i, assignmentID := range assignmentIDs {
+		infs[i+1] = assignmentID
+	}
+	bs, err := redis.ByteSlices(c.Do("HMGET", infs...))
+	if err != nil {
+		return nil, err
+	}
+	for i, b := range bs {
+		var x Assignment
+		err = dec(b, &x)
+		if err != nil {
+			return nil, err
+		}
+		x.id = assignmentIDs[i]
+		xs[i] = &x
+	}
+	return xs, nil
+}
