ee6cb11e81aee640351acc591dee366d8da82b9e
acoshift <acoshift@gmail.com>
2017-06-01 02:10:33 +0700

    refactor js

--- pkg/app/handler.go
+++ pkg/app/handler.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"net/http"
 	"strconv"
-	"strings"
 	"unicode/utf8"
 
 	"github.com/acoshift/acourse/pkg/appctx"
@@ -12,7 +11,6 @@ import (
 	"github.com/acoshift/acourse/pkg/view"
 	"github.com/acoshift/flash"
 	"github.com/acoshift/go-firebase-admin"
-	"github.com/acoshift/header"
 	"github.com/acoshift/httprouter"
 	"github.com/acoshift/session"
 	"github.com/asaskevich/govalidator"
@@ -276,142 +274,6 @@ func getSignOut(w http.ResponseWriter, r *http.Request) {
 	s := session.Get(r.Context())
 	s.Del(keyUserID)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
-}
-
-func getProfile(w http.ResponseWriter, r *http.Request) {
-	user := appctx.GetUser(r.Context())
-
-	ownCourses, err := model.ListOwnCourses(user.ID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	enrolledCourses, err := model.ListEnrolledCourses(user.ID)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	page := defaultPage
-	page.Title = user.Username + " | " + page.Title
-
-	view.Profile(w, r, &view.ProfileData{
-		Page:            &page,
-		OwnCourses:      ownCourses,
-		EnrolledCourses: enrolledCourses,
-	})
-}
-
-func getProfileEdit(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	user := appctx.GetUser(ctx)
-	f := flash.Get(ctx)
-	if !f.Has("Username") {
-		f.Set("Username", user.Username)
-	}
-	if !f.Has("Name") {
-		f.Set("Name", user.Name)
-	}
-	if !f.Has("AboutMe") {
-		f.Set("AboutMe", user.AboutMe)
-	}
-	page := defaultPage
-	page.Title = user.Username + " | " + page.Title
-	view.ProfileEdit(w, r, &view.ProfileEditData{
-		Page:  &page,
-		Flash: f,
-	})
-}
-
-func postProfileEdit(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	user := appctx.GetUser(ctx)
-	f := flash.Get(ctx)
-	if !verifyXSRF(r.FormValue("X"), user.ID, "profile-edit") {
-		f.Add("Errors", "invalid xsrf token")
-		back(w, r)
-		return
-	}
-	image, info, err := r.FormFile("Image")
-	var imageURL string
-	if err != http.ErrMissingFile {
-		if err != nil {
-			f.Add("Errors", err.Error())
-			back(w, r)
-			return
-		}
-
-		if !strings.Contains(info.Header.Get(header.ContentType), "image") {
-			f.Add("Errors", "file is not an image")
-			back(w, r)
-			return
-		}
-
-		imageURL, err = UploadProfileImage(ctx, image)
-		if err != nil {
-			f.Add("Errors", err.Error())
-			back(w, r)
-			return
-		}
-	}
-
-	var (
-		username = r.FormValue("Username")
-		name     = r.FormValue("Name")
-		aboutMe  = r.FormValue("AboutMe")
-	)
-	f.Set("Username", username)
-	f.Set("Name", name)
-	f.Set("AboutMe", aboutMe)
-
-	if !govalidator.IsAlphanumeric(username) {
-		f.Add("Errors", "username allow only a-z, A-Z, and 0-9")
-	}
-	if n := utf8.RuneCountInString(username); n < 4 || n > 32 {
-		f.Add("Errors", "username must have 4 - 32 characters")
-	}
-	if n := utf8.RuneCountInString(name); n < 4 || n > 40 {
-		f.Add("Errors", "name must have 4 - 40 characters")
-	}
-	if n := utf8.RuneCountInString(aboutMe); n > 256 {
-		f.Add("Errors", "about me must have lower than 256 characters")
-	}
-	if f.Has("Errors") {
-		back(w, r)
-		return
-	}
-
-	tx, err := db.Begin()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer tx.Rollback()
-
-	if len(imageURL) > 0 {
-		tx.Exec(`
-			update users
-			set image = $2
-			where id = $1
-		`, user.ID, imageURL)
-	}
-	tx.Exec(`
-		update users
-		set
-			username = $2,
-			name = $3,
-			about_me = $4,
-			updated_at = now()
-		where id = $1
-	`, user.ID, username, name, aboutMe)
-	err = tx.Commit()
-	if err != nil {
-		f.Add("Errors", err.Error())
-		back(w, r)
-		return
-	}
-
-	http.Redirect(w, r, "/profile", http.StatusSeeOther)
 }
 
 func getAdminUsers(w http.ResponseWriter, r *http.Request) {
