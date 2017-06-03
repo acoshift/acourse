f51f921543dfff4d1be40cbb2bf8adcd3ecb1ef8
acoshift <acoshift@gmail.com>
2017-06-03 21:06:31 +0700

    change editor contents url

---
+++ pkg/app/admin.go
@@ -0,0 +1,129 @@
+package app
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/acoshift/acourse/pkg/model"
+	"github.com/acoshift/acourse/pkg/view"
+)
+
+func getAdminUsers(w http.ResponseWriter, r *http.Request) {
+	page, _ := strconv.ParseInt(r.FormValue("page"), 10, 64)
+	if page <= 0 {
+		page = 1
+	}
+	limit := int64(30)
+
+	cnt, err := model.CountUsers()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	offset := (page - 1) * limit
+	for offset > cnt {
+		page--
+		offset = (page - 1) * limit
+	}
+	totalPage := cnt / limit
+
+	users, err := model.ListUsers(limit, offset)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	view.AdminUsers(w, r, &view.AdminUsersData{
+		Page:        &defaultPage,
+		Users:       users,
+		CurrentPage: int(page),
+		TotalPage:   int(totalPage),
+	})
+}
+
+func getAdminCourses(w http.ResponseWriter, r *http.Request) {
+	courses, err := model.ListCourses()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	view.AdminCourses(w, r, &view.AdminCoursesData{
+		Page:    &defaultPage,
+		Courses: courses,
+	})
+}
+
+func getAdminPayments(w http.ResponseWriter, r *http.Request, paymentsGetter func(int64, int64) ([]*model.Payment, error), paymentsCounter func() (int64, error)) {
+	page, _ := strconv.ParseInt(r.FormValue("page"), 10, 64)
+	if page <= 0 {
+		page = 1
+	}
+	limit := int64(30)
+
+	cnt, err := paymentsCounter()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	offset := (page - 1) * limit
+	for offset > cnt {
+		page--
+		offset = (page - 1) * limit
+	}
+	totalPage := cnt / limit
+
+	payments, err := paymentsGetter(limit, offset)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	view.AdminPayments(w, r, &view.AdminPaymentsData{
+		Page:        &defaultPage,
+		Payments:    payments,
+		CurrentPage: int(page),
+		TotalPage:   int(totalPage),
+	})
+}
+
+func postAdminPendingPayment(w http.ResponseWriter, r *http.Request) {
+	action := r.FormValue("Action")
+
+	id, err := strconv.ParseInt(r.FormValue("ID"), 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if action == "accept" {
+		x, err := model.GetPayment(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = x.Accept()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else if action == "reject" {
+		x, err := model.GetPayment(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = x.Reject()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	http.Redirect(w, r, "/admin/payments/pending", http.StatusSeeOther)
+}
+
+func getAdminPendingPayments(w http.ResponseWriter, r *http.Request) {
+	getAdminPayments(w, r, model.ListPendingPayments, model.CountPendingPayments)
+}
+
+func getAdminHistoryPayments(w http.ResponseWriter, r *http.Request) {
+	getAdminPayments(w, r, model.ListHistoryPayments, model.CountHistoryPayments)
+}
