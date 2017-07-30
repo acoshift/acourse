b1264125f9c282fe0c6cd4b65e34ae965495337e
acoshift <acoshift@gmail.com>
2017-07-30 22:02:37 +0700

    refactor, build js

--- build.js
+++ build.js
@@ -1,14 +1,26 @@
 const md5 = require('md5')
 const sass = require('node-sass')
 const fs = require('fs')
+const babel = require('babel-core')
 
-const buf = sass.renderSync({
+let config = ''
+
+let buf = sass.renderSync({
   file: 'style/main.scss',
   outputStyle: 'compressed'
 }).css
 
-const hash = md5(buf)
-const outFile = 'style.'+hash+'.css'
-
+let outFile = 'style.' + md5(buf) + '.css'
 fs.writeFileSync('static/' + outFile, buf)
-fs.writeFileSync('static.yaml', 'style.css: ' + outFile)
+config += 'style.css: ' + outFile + '\n'
+
+buf = babel.transformFileSync('script/main.js', {
+  presets: [ 'env' ],
+  minified: true,
+  compact: true
+}).code
+outFile = 'script.' + md5(buf) + '.js'
+fs.writeFileSync('static/' + outFile, buf)
+config += 'script.js: ' + outFile + '\n'
+
+fs.writeFileSync('static.yaml', config)
