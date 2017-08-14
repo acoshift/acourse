de93fd9dbb6e82378e5547211f6ab789d5c5066a
acoshift <acoshift@gmail.com>
2017-08-14 14:29:05 +0700

    model: remove unused code

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,11 +1,6 @@
 package model
 
-import (
-	"bytes"
-	"encoding/gob"
-	"errors"
-	"log"
-)
+import "errors"
 
 type scanFunc func(...interface{}) error
 
@@ -13,16 +8,3 @@ type scanFunc func(...interface{}) error
 var (
 	ErrNotFound = errors.New("not found")
 )
-
-func dec(b []byte, x interface{}) error {
-	return gob.NewDecoder(bytes.NewReader(b)).Decode(x)
-}
-
-func enc(x interface{}) []byte {
-	b := &bytes.Buffer{}
-	err := gob.NewEncoder(b).Encode(x)
-	if err != nil {
-		log.Println("enc:", err)
-	}
-	return b.Bytes()
-}
