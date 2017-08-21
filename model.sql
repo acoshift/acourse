b4fa1ee2960a0a38f286840fb7d2320e65e0ec72
acoshift <acoshift@gmail.com>
2017-08-21 22:22:06 +0700

    update sql

--- model.sql
+++ model.sql
@@ -13,9 +13,9 @@ create table users (
   updated_at timestamp not null default now(),
   primary key (id)
 );
-create unique index users_username_key on users (username);
-create unique index users_email_key on users (email);
-create index users_created_at_idx on users (created_at desc);
+create unique index on users (username);
+create unique index on users (email);
+create index on users (created_at desc);
 
 create table roles (
   user_id varchar,
@@ -26,8 +26,8 @@ create table roles (
   primary key (user_id),
   foreign key (user_id) references users (id)
 );
-create index roles_admin_idx on roles (admin);
-create index roles_instructor_idx on roles (instructor);
+create index on roles (admin);
+create index on roles (instructor);
 
 create table courses (
   id uuid default gen_random_uuid(),
@@ -47,9 +47,9 @@ create table courses (
   primary key (id),
   foreign key (user_id) references users (id)
 );
-create unique index courses_url_key on courses (url);
-create index courses_created_at_idx on courses (created_at desc);
-create index courses_updated_at_idx on courses (updated_at desc);
+create unique index on courses (url);
+create index on courses (created_at desc);
+create index on courses (updated_at desc);
 
 create table course_options (
   course_id uuid,
@@ -61,11 +61,11 @@ create table course_options (
   primary key (course_id),
   foreign key (course_id) references courses (id)
 );
-create index course_options_public_idx on course_options (public);
-create index course_options_enroll_idx on course_options (enroll);
-create index course_options_public_enroll_idx on course_options (public, enroll);
-create index course_options_public_discount_idx on course_options (public, discount);
-create index course_options_public_discount_enroll_idx on course_options (public, discount, enroll);
+create index on course_options (public);
+create index on course_options (enroll);
+create index on course_options (public, enroll);
+create index on course_options (public, discount);
+create index on course_options (public, discount, enroll);
 
 create table course_contents (
   id uuid default gen_random_uuid(),
@@ -81,7 +81,7 @@ create table course_contents (
   primary key (id),
   foreign key (course_id) references courses (id)
 );
-create index course_contents_course_id_i_idx on course_contents (course_id, i);
+create index on course_contents (course_id, i);
 
 create table assignments (
   id uuid default gen_random_uuid(),
@@ -95,7 +95,7 @@ create table assignments (
   primary key (id),
   foreign key (course_id) references courses (id)
 );
-create index assignments_course_id_idx on assignments (course_id, i);
+create index on assignments (course_id, i);
 
 create table user_assignments (
   id uuid default gen_random_uuid(),
@@ -107,7 +107,7 @@ create table user_assignments (
   foreign key (user_id) references users (id),
   foreign key (assignment_id) references assignments (id)
 );
-create index user_assignments_created_at_idx on user_assignments (created_at);
+create index on user_assignments (created_at);
 
 create table enrolls (
   user_id varchar,
@@ -117,9 +117,9 @@ create table enrolls (
   foreign key (user_id) references users (id),
   foreign key (course_id) references courses (id)
 );
-create index enrolls_created_at_idx on enrolls (created_at);
-create index enrolls_user_id_created_at_idx on enrolls (user_id, created_at);
-create index enrolls_course_id_created_at_idx on enrolls (course_id, created_at);
+create index on enrolls (created_at);
+create index on enrolls (user_id, created_at);
+create index on enrolls (course_id, created_at);
 
 create table attends (
   id uuid default gen_random_uuid(),
@@ -130,10 +130,10 @@ create table attends (
   foreign key (user_id) references users (id),
   foreign key (course_id) references courses (id)
 );
-create index attends_created_at_idx on attends (created_at);
-create index attends_user_id_created_at_idx on attends (user_id, created_at);
-create index attends_course_id_created_at_idx on attends (course_id, created_at);
-create index attends_user_id_course_id_created_at_idx on attends (user_id, course_id, created_at);
+create index on attends (created_at);
+create index on attends (user_id, created_at);
+create index on attends (course_id, created_at);
+create index on attends (user_id, course_id, created_at);
 
 create table payments (
   id uuid default gen_random_uuid(),
@@ -151,7 +151,7 @@ create table payments (
   foreign key (user_id) references users (id),
   foreign key (course_id) references courses (id)
 );
-create index payments_created_at_idx on payments (created_at desc);
-create index payments_code_idx on payments (code);
-create index payments_course_id_code_idx on payments (course_id, code);
-create index payments_status_created_at_idx on payments (status, created_at desc);
+create index on payments (created_at desc);
+create index on payments (code);
+create index on payments (course_id, code);
+create index on payments (status, created_at desc);
