be1a10667f96cf554285521135dfdce6457ee34f
acoshift <acoshift@gmail.com>
2018-03-14 20:48:45 +0700

    update dep

--- vendor/github.com/acoshift/methodmux/methodmux.go
+++ vendor/github.com/acoshift/methodmux/methodmux.go
@@ -24,6 +24,7 @@ func (m Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// fallback
 	if h := m[""]; h != nil {
 		h.ServeHTTP(w, r)
+		return
 	}
 
 	// handler not found, returns method not allowed
