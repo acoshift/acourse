8cfe81cd0286d51e907c1ba3be819c7b6c3cacea
acoshift <acoshift@gmail.com>
2018-08-16 07:47:38 +0700

    update readme, add license, format table.sql

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # acourse
 
 Acoshift's course system
+
+## Development
+
+### Config
+
+- add config/sql_url to your postgres database
+- add config/service_account to your gcloud service account (using for upload storage)
+- add gcloud project id to config/project_id
+- add gcloud bucket name to config/bucket
+- add smtp email config to config/email_{from,password,port,server,user}
+
+### Software
+
+- [PostgreSQL](https://www.postgresql.org/)
+- [Redis](https://redis.io/)
+- [goreload](https://github.com/acoshift/goreload)
+- [Node.js LTS](https://nodejs.org/)
+
+### Running
+
+- go get -u github.com/acoshift/goreload
+- yarn install
+- make style
+- make dev
+
+## Deployment
+
+After GCB finished (and live in staging), run `git push origin HEAD:production` to trigger production deployment.
+
+## License
+
+Apache-2.0
--- table.sql
+++ table.sql
@@ -1,153 +1,155 @@
-CREATE TABLE users (
-    id          VARCHAR NOT NULL,
-    username    VARCHAR NOT NULL,
-    name        VARCHAR NOT NULL,
-    email       VARCHAR,
-    about_me    VARCHAR NOT NULL DEFAULT '',
-    image       VARCHAR NOT NULL DEFAULT '',
-    created_at  TIMESTAMP NOT NULL DEFAULT now(),
-    updated_at  TIMESTAMP NOT NULL DEFAULT now(),
-    PRIMARY KEY (id)
-);
-CREATE UNIQUE INDEX ON users (username);
-CREATE UNIQUE INDEX ON users (email);
-CREATE INDEX ON users (created_at DESC);
+create extension if not exists pgcrypto;
 
-CREATE TABLE roles (
-  user_id       VARCHAR,
-  admin         BOOL NOT NULL DEFAULT FALSE,
-  instructor    BOOL NOT NULL DEFAULT FALSE,
-  created_at    TIMESTAMP NOT NULL DEFAULT now(),
-  updated_at    TIMESTAMP NOT NULL DEFAULT now(),
-  PRIMARY KEY (user_id),
-  FOREIGN KEY (user_id) REFERENCES users (id)
+create table users (
+	id varchar not null,
+	username varchar not null,
+	name varchar not null,
+	email varchar,
+	about_me varchar not null default '',
+	image varchar not null default '',
+	created_at timestamp not null default now(),
+	updated_at timestamp not null default now(),
+	primary key (id)
 );
-CREATE INDEX ON roles (admin);
-CREATE INDEX ON roles (instructor);
+create unique index on users (username);
+create unique index on users (email);
+create index on users (created_at desc);
 
-CREATE TABLE courses (
-  id            UUID DEFAULT gen_random_uuid(),
-  user_id       VARCHAR NOT NULL,
-  title         VARCHAR NOT NULL,
-  short_desc    VARCHAR NOT NULL,
-  long_desc     VARCHAR NOT NULL,
-  image         VARCHAR NOT NULL,
-  start         TIMESTAMP DEFAULT NULL,
-  url           VARCHAR DEFAULT NULL,
-  type          INT NOT NULL DEFAULT 0,
-  price         DECIMAL(9,2) NOT NULL DEFAULT 0,
-  discount      DECIMAL(9,2) DEFAULT 0,
-  enroll_detail VARCHAR NOT NULL DEFAULT '',
-  created_at    TIMESTAMP NOT NULL DEFAULT now(),
-  updated_at    TIMESTAMP NOT NULL DEFAULT now(),
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users (id)
+create table roles (
+	user_id varchar,
+	admin bool not null default false,
+	instructor bool not null default false,
+	created_at timestamp not null default now(),
+	updated_at timestamp not null default now(),
+	primary key (user_id),
+	foreign key (user_id) references users (id)
 );
-CREATE UNIQUE INDEX ON courses (url);
-CREATE INDEX ON courses (created_at DESC);
-CREATE INDEX ON courses (updated_at DESC);
+create index on roles (admin);
+create index on roles (instructor);
 
-CREATE TABLE course_options (
-  course_id     UUID,
-  public        BOOL NOT NULL DEFAULT FALSE,
-  enroll        BOOL NOT NULL DEFAULT FALSE,
-  attend        BOOL NOT NULL DEFAULT FALSE,
-  assignment    BOOL NOT NULL DEFAULT FALSE,
-  discount      BOOL NOT NULL DEFAULT FALSE,
-  PRIMARY KEY (course_id),
-  FOREIGN KEY (course_id) REFERENCES courses (id)
+create table courses (
+	id uuid default gen_random_uuid(),
+	user_id varchar not null,
+	title varchar not null,
+	short_desc varchar not null,
+	long_desc varchar not null,
+	image varchar not null,
+	start timestamp default null,
+	url varchar default null,
+	type int not null default 0,
+	price decimal(9,2) not null default 0,
+	discount decimal(9,2) default 0,
+	enroll_detail varchar not null default '',
+	created_at timestamp not null default now(),
+	updated_at timestamp not null default now(),
+	primary key (id),
+	foreign key (user_id) references users (id)
 );
-CREATE INDEX ON course_options (public);
-CREATE INDEX ON course_options (enroll);
-CREATE INDEX ON course_options (public, enroll);
-CREATE INDEX ON course_options (public, discount);
-CREATE INDEX ON course_options (public, discount, enroll);
+create unique index on courses (url);
+create index on courses (created_at desc);
+create index on courses (updated_at desc);
 
-CREATE TABLE course_contents (
-  id            UUID DEFAULT gen_random_uuid(),
-  course_id     UUID NOT NULL,
-  i             INT NOT NULL DEFAULT 0,
-  title         VARCHAR NOT NULL DEFAULT '',
-  long_desc     VARCHAR NOT NULL DEFAULT '',
-  video_id      VARCHAR NOT NULL DEFAULT '',
-  video_type    INT NOT NULL DEFAULT 0,
-  download_url  VARCHAR NOT NULL DEFAULT '',
-  created_at    TIMESTAMP NOT NULL DEFAULT now(),
-  updated_at    TIMESTAMP NOT NULL DEFAULT now(),
-  PRIMARY KEY (id),
-  FOREIGN KEY (course_id) REFERENCES courses (id)
+create table course_options (
+	course_id uuid,
+	public bool not null default false,
+	enroll bool not null default false,
+	attend bool not null default false,
+	assignment bool not null default false,
+	discount bool not null default false,
+	primary key (course_id),
+	foreign key (course_id) references courses (id)
 );
-CREATE INDEX ON course_contents (course_id, i);
+create index on course_options (public);
+create index on course_options (enroll);
+create index on course_options (public, enroll);
+create index on course_options (public, discount);
+create index on course_options (public, discount, enroll);
 
-CREATE TABLE assignments (
-  id            UUID DEFAULT gen_random_uuid(),
-  course_id     UUID NOT NULL,
-  i             INT NOT NULL,
-  title         VARCHAR NOT NULL,
-  long_desc     VARCHAR NOT NULL,
-  open          BOOL NOT NULL DEFAULT FALSE,
-  created_at    TIMESTAMP NOT NULL DEFAULT now(),
-  updated_at    TIMESTAMP NOT NULL DEFAULT now(),
-  PRIMARY KEY (id),
-  FOREIGN KEY (course_id) REFERENCES courses (id)
+create table course_contents (
+	id uuid default gen_random_uuid(),
+	course_id uuid not null,
+	i int not null default 0,
+	title varchar not null default '',
+	long_desc varchar not null default '',
+	video_id varchar not null default '',
+	video_type int not null default 0,
+	download_url varchar not null default '',
+	created_at timestamp not null default now(),
+	updated_at timestamp not null default now(),
+	primary key (id),
+	foreign key (course_id) references courses (id)
 );
-CREATE INDEX ON assignments (course_id, i);
+create index on course_contents (course_id, i);
 
-CREATE TABLE user_assignments (
-  id            UUID DEFAULT gen_random_uuid(),
-  user_id       VARCHAR NOT NULL,
-  assignment_id UUID NOT NULL,
-  download_url  VARCHAR NOT NULL,
-  created_at    TIMESTAMP NOT NULL DEFAULT now(),
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (assignment_id) REFERENCES assignments (id)
+create table assignments (
+	id uuid default gen_random_uuid(),
+	course_id uuid not null,
+	i int not null,
+	title varchar not null,
+	long_desc varchar not null,
+	open bool not null default false,
+	created_at timestamp not null default now(),
+	updated_at timestamp not null default now(),
+	primary key (id),
+	foreign key (course_id) references courses (id)
 );
-CREATE INDEX ON user_assignments (created_at);
+create index on assignments (course_id, i);
 
-CREATE TABLE enrolls (
-  user_id       VARCHAR,
-  course_id     UUID NOT NULL,
-  created_at    TIMESTAMP NOT NULL DEFAULT now(),
-  PRIMARY KEY (user_id, course_id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (course_id) REFERENCES courses (id)
+create table user_assignments (
+	id uuid default gen_random_uuid(),
+	user_id varchar not null,
+	assignment_id uuid not null,
+	download_url varchar not null,
+	created_at timestamp not null default now(),
+	primary key (id),
+	foreign key (user_id) references users (id),
+	foreign key (assignment_id) references assignments (id)
 );
-CREATE INDEX ON enrolls (created_at);
-CREATE INDEX ON enrolls (user_id, created_at);
-CREATE INDEX ON enrolls (course_id, created_at);
+create index on user_assignments (created_at);
 
-CREATE TABLE attends (
-  id            UUID DEFAULT gen_random_uuid(),
-  user_id       VARCHAR NOT NULL,
-  course_id     UUID NOT NULL,
-  created_at    TIMESTAMP NOT NULL DEFAULT now(),
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (course_id) REFERENCES courses (id)
+create table enrolls (
+	user_id varchar,
+	course_id uuid not null,
+	created_at timestamp not null default now(),
+	primary key (user_id, course_id),
+	foreign key (user_id) references users (id),
+	foreign key (course_id) references courses (id)
 );
-CREATE INDEX ON attends (created_at);
-CREATE INDEX ON attends (user_id, created_at);
-CREATE INDEX ON attends (course_id, created_at);
-CREATE INDEX ON attends (user_id, course_id, created_at);
+create index on enrolls (created_at);
+create index on enrolls (user_id, created_at);
+create index on enrolls (course_id, created_at);
 
-CREATE TABLE payments (
-  id                UUID DEFAULT gen_random_uuid(),
-  user_id           VARCHAR NOT NULL,
-  course_id         UUID NOT NULL,
-  image             VARCHAR NOT NULL,
-  price             DECIMAL(9, 2) NOT NULL,
-  original_price    DECIMAL(9, 2) NOT NULL,
-  code              VARCHAR NOT NULL,
-  status            INT NOT NULL,
-  created_at        TIMESTAMP NOT NULL DEFAULT now(),
-  updated_at        TIMESTAMP NOT NULL DEFAULT now(),
-  at                TIMESTAMP DEFAULT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users (id),
-  FOREIGN KEY (course_id) REFERENCES courses (id)
+create table attends (
+	id uuid default gen_random_uuid(),
+	user_id varchar not null,
+	course_id uuid not null,
+	created_at timestamp not null default now(),
+	primary key (id),
+	foreign key (user_id) references users (id),
+	foreign key (course_id) references courses (id)
 );
-CREATE INDEX ON payments (created_at DESC);
-CREATE INDEX ON payments (code);
-CREATE INDEX ON payments (course_id, code);
-CREATE INDEX ON payments (status, created_at DESC);
+create index on attends (created_at);
+create index on attends (user_id, created_at);
+create index on attends (course_id, created_at);
+create index on attends (user_id, course_id, created_at);
+
+create table payments (
+	id uuid default gen_random_uuid(),
+	user_id varchar not null,
+	course_id uuid not null,
+	image varchar not null,
+	price decimal(9, 2) not null,
+	original_price decimal(9, 2) not null,
+	code varchar not null,
+	status int not null,
+	created_at timestamp not null default now(),
+	updated_at timestamp not null default now(),
+	at timestamp default null,
+	primary key (id),
+	foreign key (user_id) references users (id),
+	foreign key (course_id) references courses (id)
+);
+create index on payments (created_at desc);
+create index on payments (code);
+create index on payments (course_id, code);
+create index on payments (status, created_at desc);
