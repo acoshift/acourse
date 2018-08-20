d2ada4ebd8388471c31bd6c06b9ddba82a05e1f2
Thanatat Tamtan <acoshift@gmail.com>
2018-08-20 20:42:47 +0700

    add tests :D (#176)
    * test/service: add sign up
    
    * test/service: sign up tests

--- service/auth_test.go
+++ service/auth_test.go
@@ -3,12 +3,14 @@ package service_test
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	"github.com/acoshift/go-firebase-admin"
 
+	"github.com/acoshift/acourse/entity"
 	. "github.com/acoshift/acourse/service"
 )
 
@@ -27,6 +29,131 @@ var _ = Describe("Auth", func() {
 		s = New(Config{
 			Repository: repo,
 			Auth:       auth,
+		})
+	})
+
+	Describe("SignUp", func() {
+		It("should error with zero value email", func() {
+			userID, err := s.SignUp(ctx, "", "123456")
+
+			Expect(err).NotTo(BeNil())
+			Expect(IsUIError(err)).To(BeTrue())
+			Expect(userID).To(BeZero())
+		})
+
+		It("should error with invalid email", func() {
+			userID, err := s.SignUp(ctx, "invalid email", "123456")
+
+			Expect(err).NotTo(BeNil())
+			Expect(IsUIError(err)).To(BeTrue())
+			Expect(userID).To(BeZero())
+		})
+
+		It("should error with zero value password", func() {
+			userID, err := s.SignUp(ctx, "test@test.com", "")
+
+			Expect(err).NotTo(BeNil())
+			Expect(IsUIError(err)).To(BeTrue())
+			Expect(userID).To(BeZero())
+		})
+
+		It("should error with too short password", func() {
+			userID, err := s.SignUp(ctx, "test@test.com", "123")
+
+			Expect(err).NotTo(BeNil())
+			Expect(IsUIError(err)).To(BeTrue())
+			Expect(userID).To(BeZero())
+		})
+
+		It("should error with too long password", func() {
+			userID, err := s.SignUp(ctx, "test@test.com", strings.Repeat("1", 100))
+
+			Expect(err).NotTo(BeNil())
+			Expect(IsUIError(err)).To(BeTrue())
+			Expect(userID).To(BeZero())
+		})
+
+		It("should propagate error when firebase error", func() {
+			auth.On("CreateUser", &firebase.User{
+				Email:    "test@test.com",
+				Password: "12345678",
+			}).Return("", fmt.Errorf("error"))
+
+			userID, err := s.SignUp(ctx, "test@test.com", "12345678")
+
+			Expect(err).NotTo(BeNil())
+			Expect(IsUIError(err)).To(BeTrue())
+			Expect(userID).To(BeZero())
+		})
+
+		It("should error when email not available", func() {
+			auth.On("CreateUser", &firebase.User{
+				Email:    "test@test.com",
+				Password: "12345678",
+			}).Return("123", nil)
+			repo.On("RegisterUser", &RegisterUser{
+				ID:       "123",
+				Username: "123",
+				Email:    "test@test.com",
+			}).Return(entity.ErrEmailNotAvailable)
+
+			userID, err := s.SignUp(ctx, "test@test.com", "12345678")
+
+			Expect(err).NotTo(BeNil())
+			Expect(IsUIError(err)).To(BeTrue())
+			Expect(userID).To(BeZero())
+		})
+
+		It("should error when username not available", func() {
+			auth.On("CreateUser", &firebase.User{
+				Email:    "test@test.com",
+				Password: "12345678",
+			}).Return("123", nil)
+			repo.On("RegisterUser", &RegisterUser{
+				ID:       "123",
+				Username: "123",
+				Email:    "test@test.com",
+			}).Return(entity.ErrUsernameNotAvailable)
+
+			userID, err := s.SignUp(ctx, "test@test.com", "12345678")
+
+			Expect(err).NotTo(BeNil())
+			Expect(IsUIError(err)).To(BeTrue())
+			Expect(userID).To(BeZero())
+		})
+
+		It("should error when database return error", func() {
+			auth.On("CreateUser", &firebase.User{
+				Email:    "test@test.com",
+				Password: "12345678",
+			}).Return("123", nil)
+			repo.On("RegisterUser", &RegisterUser{
+				ID:       "123",
+				Username: "123",
+				Email:    "test@test.com",
+			}).Return(fmt.Errorf("db error"))
+
+			userID, err := s.SignUp(ctx, "test@test.com", "12345678")
+
+			Expect(err).NotTo(BeNil())
+			Expect(userID).To(BeZero())
+		})
+
+		It("should return user id when success", func() {
+			auth.On("CreateUser", &firebase.User{
+				Email:    "test@test.com",
+				Password: "12345678",
+			}).Return("123", nil)
+			repo.On("RegisterUser", &RegisterUser{
+				ID:       "123",
+				Username: "123",
+				Email:    "test@test.com",
+			}).Return(nil)
+
+			userID, err := s.SignUp(ctx, "test@test.com", "12345678")
+
+			Expect(err).To(BeNil())
+			Expect(userID).To(Equal("123"))
 		})
 	})
 
