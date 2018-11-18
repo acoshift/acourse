6343940293028ee2ff24476b3f927b6950e13ce4
acoshift <acoshift@gmail.com>
2018-11-19 00:44:29 +0700

    chore: cleanup

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -1,32 +1,5 @@
 package entity
 
-import (
-	"time"
-
-	"github.com/lib/pq"
-
-	"github.com/acoshift/acourse/internal/pkg/model/course"
-	"github.com/acoshift/acourse/internal/pkg/model/user"
-)
-
-// Payment model
-type Payment struct {
-	ID            string
-	UserID        string
-	CourseID      string
-	Image         string
-	Price         float64
-	OriginalPrice float64
-	Code          string
-	Status        int
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
-	At            pq.NullTime
-
-	User   user.User
-	Course course.Course
-}
-
 // Assignment model
 type Assignment struct {
 	ID    string
