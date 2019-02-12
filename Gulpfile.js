19dac45c7c44e8087816faecd0c1e6fed5093a1e
anthoz69 <st.anthoz69@gmail.com>
2019-02-12 17:02:30 +0700

    add biomatic

--- Gulpfile.js
+++ Gulpfile.js
@@ -15,7 +15,11 @@ const sassOption = {
 	includePaths: 'node_modules'
 }
 
-gulp.task('default', sync(['style', 'static']))
+gulp.task('default', sync(['style', 'atomic', 'static']))
+
+gulp.task('watch', function () {
+  return gulp.watch('./style/**/*.scss', ['default'])
+})
 
 gulp.task('style', () => gulp
 	.src('./style/main.scss')
@@ -27,8 +31,19 @@ gulp.task('style', () => gulp
 	.pipe(gulp.dest('./assets'))
 )
 
+gulp.task('atomic', () => gulp
+	.src('./style/atomic.scss')
+	.pipe(sass(sassOption).on('error', sass.logError))
+	.pipe(concat('atomic.css'))
+	// .pipe(prod ? purgecss({ content: ['template/**/*.tmpl'] }) : noop())
+	.pipe(prod ? freeze() : noop())
+	.pipe(filenames('atomic'))
+	.pipe(gulp.dest('./assets'))
+)
+
 gulp.task('static', async () => {
 	await fs.mkdirp('.build')
 	await fs.remove('.build/static.yaml')
-	await fs.appendFile('.build/static.yaml', 'style.css: ' + filenames.get('style')[0] + '\n')
+  await fs.appendFile('.build/static.yaml', `style.css: ${filenames.get('style')[0]}\n`)
+  await fs.appendFile('.build/static.yaml', `atomic.css: ${filenames.get('atomic')[0]}\n`)
 })
